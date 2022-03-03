f20c4e076d74f3dd0563de64212a701c1ad7d986
Przemyslaw Majewski <przemmekm@gmail.com>
2022-03-03 22:50:27 +0100

    adding functions

--- javascript/theme-button.js
+++ javascript/theme-button.js
@@ -1,16 +1,27 @@
-console.log("Hello!");
-
-let themeButton = document.querySelector(".js-themeButton");
-let siteBackground = document.querySelector(".js-siteBackground");
-let themeName = document.querySelector(".js-themeName");
-
-themeButton.addEventListener("click", () => {
-    siteBackground.classList.toggle("darkTheme");
-    themeButton.classList.toggle("darkTheme");
-
-    if (siteBackground.classList.contains("darkTheme")) {
-        themeName.innerText = "light";
-    } else {
-        themeName.innerText = "dark";
+{
+    const welcome = () => {
+        console.log("Hello!");
     }
-});
+
+    const themeButton = document.querySelector(".js-themeButton");
+
+    const toggleBackground = () => {
+        const siteBackground = document.querySelector(".js-siteBackground");
+        const themeName = document.querySelector(".js-themeName");
+
+        siteBackground.classList.toggle("darkTheme");
+        themeButton.classList.toggle("darkTheme");
+
+        if (siteBackground.classList.contains("darkTheme")) {
+            themeName.innerText = "light";
+        } else {
+            themeName.innerText = "dark";
+        }
+    }
+    const init = () => {
+        themeButton.addEventListener("click", toggleBackground)
+
+        welcome();
+    }
+    init();
+}
